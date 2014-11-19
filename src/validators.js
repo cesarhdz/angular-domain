183af4ddb21fd838f9adb94481d8e342123e87ef
cesarhdz <mrshortcuts@gmail.com>
2014-11-19 07:25:12 -0600

    Validators added

---
+++ src/validators.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+angular.module('domain')
+
+	.filter('isString', function(){
+	
+		return function(str){
+
+		}
+
+	})
+
+
+
+	.filter('maxLength', function(){
+
+		return function(target, max){
+			
+		}
+	})
+
